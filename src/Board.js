16234ef16393b22ad0d184a14466cbc69c401039
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-09-28 21:17:09 +0300

    Board: add #isEmpty

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,10 @@
 'use strict'
 
 module.exports = class Board {
+  constructor() {
+  }
 
+  isEmptyAt(coords) {
+    return true
+  }
 }
--- test/Board.test.js
+++ test/Board.test.js
@@ -5,7 +5,20 @@ const should = require('chai').should()
 const Board = require('../src/Board')
 
 suite('Board', () => {
-  test('should exist', () => {
-    should.exist(Board)
+  let emptyBoard
+  setup(() => {
+    emptyBoard = new Board()
+  })
+
+  suite('initially', () => {
+    test('is empty at top left position', () => {
+      const topLeftCoords = [0, 0]
+      emptyBoard.isEmptyAt(topLeftCoords).should.equal(true)
+    })
+
+    test('is empty at bottom right position', () => {
+      const bottomRightCoords = [2, 2]
+      emptyBoard.isEmptyAt(bottomRightCoords).should.equal(true)
+    })
   })
 })
