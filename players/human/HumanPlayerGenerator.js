3406a4958786f63fd438af71c354aecda899bdee
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-10-17 17:47:45 +0300

    add HumanPlayer generator

---
+++ players/human/HumanPlayerGenerator.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const _ = require('lodash')
+
+const InteractiveMoveAdviser = require('../../advisers/interactive/index'),
+  Player = require('../../core').Player
+
+module.exports = HumanPlayersGenerator
+
+function HumanPlayersGenerator(inputProcessor) {
+  return class HumanPlayer {
+    constructor(sign, name) {
+      this.name = makeName(sign, name)
+      this.gamePlayer = makeGamePlayer(sign, inputProcessor)
+    }
+
+    willChooseCoordsFor(board) {
+      return this.gamePlayer.willChooseCoordsFor(board)
+    }
+
+    fillCellOnBoard(board) {
+      return this.gamePlayer.fillCellOnBoard(board)
+    }
+
+    toString() {
+      return `${this.name} [H]`
+    }
+  }
+}
+
+function makeName(sign, name) {
+  return (_.isEmpty(name)) ? sign : `${name} (${sign})`
+}
+
+function makeGamePlayer(sign, inputProcessor) {
+  let moveAdviser = new InteractiveMoveAdviser(inputProcessor)
+  return new Player(sign, moveAdviser)
+}
