451391b8a391c07c910aca1510ffdd7a056708f1
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-10-30 00:04:46 +0200

    cleanup unused method

--- formatters/src/table/format/index.js
+++ formatters/src/table/format/index.js
@@ -3,22 +3,7 @@
 const SimpleTableFormat = require('./SimpleTableFormat')
 
 module.exports = makeSimpleFormat
-makeSimpleFormat.solid = makeSolidFormat
 
 function makeSimpleFormat(settings) {
-  return new SimpleTableFormat(settings)
-}
-
-function makeSolidFormat(settings) {
-  const defaults = {
-    padding: 1,
-    verticalSeparator: '|',
-    border: {
-      top: '-',
-      bottom: '-',
-      inner: '-'
-    }
-  }
-  settings = Object.assign({}, defaults, settings)
   return new SimpleTableFormat(settings)
 }
