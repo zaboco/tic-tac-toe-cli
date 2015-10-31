5788285f2ca0a59bb95b697ed6a91e9fa17823a5
Bogdan Zaharia <bogdan.zaharia@kalon.ro>
2015-10-31 11:06:37 +0200

    bestOutcomeWhenChosing: add tests for win & tie at 3 empty cells

--- solver/test/SmartSolverAdviser.test.js
+++ solver/test/SmartSolverAdviser.test.js
@@ -12,17 +12,15 @@ suite('advisers/smartSolver', () => {
   suite('calculateBestOutcomeForMove', () => {
     let board
 
+    test('it throws error when receiving winning board', () => {
+      board = Board.prefilled.fromRow([X, X, X])
+      ChoiceOptimizer(board).bestOutcomeWhenChoosing.bind(null, [2, 1])
+        .should.throw(/finished/i)
+    })
+
+    const bottomLeft = [2, 0], bottomCenter = [2, 1], bottomRight = [2, 2]
     suite('for only one empty cell', () => {
       const currentSign = 'O'
-      test('it throws error when receiving winning board', () => {
-        board = Board.prefilled.fromMatrix([
-          [X, X, X],
-          [O, O, X],
-          [X, _, O]
-        ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing.bind(null, [2, 1])
-          .should.throw(/finished/i)
-      })
 
       test('it is win if, by filling the cell, the current sign wins', () => {
         board = Board.prefilled.fromMatrix([
@@ -30,7 +28,7 @@ suite('advisers/smartSolver', () => {
           [O, O, X],
           [X, _, O]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing([2, 1]).should.equal(WIN)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomCenter).should.equal(WIN)
       })
 
       test('it is tie if, by filling the cell, the game ends with a tie', () => {
@@ -39,52 +37,67 @@ suite('advisers/smartSolver', () => {
           [O, X, X],
           [X, _, O]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing([2, 1]).should.equal(TIE)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomCenter).should.equal(TIE)
       })
     })
 
     suite('for two empty cells on board', () => {
       const currentSign = 'X'
       test('it is win if immediate winning move', () => {
-        let chosenCoords = [2, 2]
         board = Board.prefilled.fromMatrix([
           [X, O, X],
           [O, O, X],
           [O, _, _]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(chosenCoords).should.equal(WIN)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomRight).should.equal(WIN)
       })
 
       test('it is tie if opponent`s next move will end the game as such', () => {
-        let chosenCoords = [2, 1]
         board = Board.prefilled.fromMatrix([
           [X, O, X],
           [O, O, X],
           [O, _, _]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(chosenCoords).should.equal(TIE)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomCenter).should.equal(TIE)
       })
 
       test('it is loss if opponent`s next move will win', () => {
-        let chosenCoords = [2, 2]
         board = Board.prefilled.fromMatrix([
           [X, O, O],
           [O, O, X],
           [X, _, _]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(chosenCoords).should.equal(LOSS)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomRight).should.equal(LOSS)
       })
     })
 
     suite('for three empty cells on board', () => {
+      const currentSign = 'O'
       test('it is loss if opponent has a winning next move', () => {
-        let currentSign = 'O', chosenCoords = [2, 0]
         board = Board.prefilled.fromMatrix([
           [X, O, O],
           [O, X, X],
           [_, _, _]
         ])
-        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(chosenCoords).should.equal(LOSS)
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomLeft).should.equal(LOSS)
+      })
+
+      test('it is tie if opponent has a no winning move', () => {
+        board = Board.prefilled.fromMatrix([
+          [X, O, O],
+          [O, X, X],
+          [_, _, _]
+        ])
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomRight).should.equal(TIE)
+      })
+
+      test('it is win if opponent has a no chance of avoiding it', () => {
+        board = Board.prefilled.fromMatrix([
+          [O, O, X],
+          [_, X, O],
+          [_, _, O]
+        ])
+        ChoiceOptimizer(board, currentSign).bestOutcomeWhenChoosing(bottomLeft).should.equal(WIN)
       })
     })
   })
